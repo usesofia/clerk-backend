0eb31283201b5aa982d07df66b876de79ba5a990
Marco Prado <marco@usesofia.com>
2025-01-20 13:57:11 -0300

    Enhance OrganizationAPI and UsersAPI with new methods and improved type definitions
    - Added new methods to OrganizationAPI for managing organization memberships and invitations, including `createOrganizationMembership`, `deleteOrganizationMembership`, `updateOrganizationMembership`, and `getOrganizationInvitationList`.
    - Updated existing methods to return more specific types, such as `Promise<Organization>` and `Promise<OrganizationInvitation>`, improving type safety.
    - Introduced new methods in UsersAPI for user management, including `createUser` and `getUserList`, with appropriate type definitions for better clarity and usability.
    - Enhanced logging throughout the APIs to provide better traceability of operations and errors.

--- src/organizations-api.ts
+++ src/organizations-api.ts
@@ -1,10 +1,14 @@
 import {
     ClerkClient as _ClerkClient,
+    Organization,
+    OrganizationInvitation,
+    OrganizationMembership,
     OrganizationMembershipRole,
 } from '@clerk/backend';
 import { retryStatuses, retryOptions } from './utils';
 import * as retry from 'retry';
 import { ClerkLogger } from './clerk-logger.interface';
+import { PaginatedResourceResponse } from '@clerk/backend/dist/api/resources/Deserializer';
 
 export class OrganizationAPI {
     private client: _ClerkClient;
@@ -19,7 +23,7 @@ export class OrganizationAPI {
         organizationId,
     }: {
         organizationId: string;
-    }) {
+    }): Promise<Organization> {
         const functionName = 'clerk.organizations.getOrganization';
         this.logger.logInput({
             functionName,
@@ -69,7 +73,7 @@ export class OrganizationAPI {
         role: OrganizationMembershipRole;
         redirectUrl?: string;
         publicMetadata?: OrganizationInvitationPublicMetadata;
-    }) {
+    }): Promise<OrganizationInvitation> {
         const functionName = 'clerk.organizations.createOrganizationInvitation';
         this.logger.logInput({
             functionName,
@@ -80,12 +84,12 @@ export class OrganizationAPI {
         return new Promise((resolve, reject) => {
             operation.attempt(async (currentAttempt) => {
                 try {
-                    const organization = await this.client.organizations.createOrganizationInvitation({ organizationId, inviterUserId, emailAddress, role, redirectUrl, publicMetadata });
+                    const organizationInvitation = await this.client.organizations.createOrganizationInvitation({ organizationId, inviterUserId, emailAddress, role, redirectUrl, publicMetadata });
                     this.logger.logOutput({
                         functionName,
-                        output: organization,
+                        output: organizationInvitation,
                     });
-                    resolve(organization);
+                    resolve(organizationInvitation);
                 } catch(error) {
                     if(operation.retry(error) && error.status && retryStatuses.includes(error.status)) {
                         this.logger.logRetryError({
@@ -113,7 +117,7 @@ export class OrganizationAPI {
         organizationId: string;
         limit?: number;
         offset?: number;
-    }) {
+    }): Promise<PaginatedResourceResponse<OrganizationMembership[]>> {
         const functionName = 'clerk.organizations.getOrganizationMembershipList';
         this.logger.logInput({
             functionName,
@@ -124,12 +128,12 @@ export class OrganizationAPI {
         return new Promise((resolve, reject) => {
             operation.attempt(async (currentAttempt) => {
                 try {
-                    const organizationsPage = await this.client.organizations.getOrganizationMembershipList({ organizationId, limit, offset });
+                    const organizationMembershipsPage = await this.client.organizations.getOrganizationMembershipList({ organizationId, limit, offset });
                     this.logger.logOutput({
                         functionName,
-                        output: organizationsPage,
+                        output: organizationMembershipsPage,
                     });
-                    resolve(organizationsPage);
+                    resolve(organizationMembershipsPage);
                 } catch(error) {
                     if(operation.retry(error) && error.status && retryStatuses.includes(error.status)) {
                         this.logger.logRetryError({
@@ -168,12 +172,408 @@ export class OrganizationAPI {
         return new Promise((resolve, reject) => {
             operation.attempt(async (currentAttempt) => {
                 try {
-                    const organization = await this.client.organizations.revokeOrganizationInvitation({ organizationId, invitationId, requestingUserId });
+                    const organizationInvitation = await this.client.organizations.revokeOrganizationInvitation({ organizationId, invitationId, requestingUserId });
+                    this.logger.logOutput({
+                        functionName,
+                        output: organizationInvitation,
+                    });
+                    resolve(organizationInvitation);
+                } catch(error) {
+                    if(operation.retry(error) && error.status && retryStatuses.includes(error.status)) {
+                        this.logger.logRetryError({
+                            functionName,
+                            currentAttempt,
+                            error,
+                        });
+                        return;
+                    }
+                    this.logger.logError({
+                        functionName,
+                        error,
+                    });
+                    reject(operation.mainError());
+                }
+            });
+        });
+    }
+
+    public async getOrganizationInvitationList({
+        organizationId,
+        limit,
+        offset,
+    }: {
+        organizationId: string;
+        limit?: number;
+        offset?: number;
+    }) {
+        const functionName = 'clerk.organizations.getOrganizationInvitationList';
+        this.logger.logInput({
+            functionName,
+            args: [{organizationId, limit, offset}],
+        });
+        const operation = retry.operation(retryOptions);
+
+        return new Promise((resolve, reject) => {
+            operation.attempt(async (currentAttempt) => {
+                try {
+                    const invitationsPage = await this.client.organizations.getOrganizationInvitationList({ organizationId, limit, offset });
+                    this.logger.logOutput({
+                        functionName,
+                        output: invitationsPage,
+                    });
+                    resolve(invitationsPage);
+                } catch(error) {
+                    if(operation.retry(error) && error.status && retryStatuses.includes(error.status)) {
+                        this.logger.logRetryError({
+                            functionName,
+                            currentAttempt,
+                            error,
+                        });
+                        return;
+                    }
+                    this.logger.logError({
+                        functionName,
+                        error,
+                    });
+                    reject(operation.mainError());
+                }
+            });
+        });
+    }
+
+    public async deleteOrganizationMembership({
+        organizationId,
+        userId,
+    }: {
+        organizationId: string;
+        userId: string;
+    }) {
+        const functionName = 'clerk.organizations.deleteOrganizationMembership';
+        this.logger.logInput({
+            functionName,
+            args: [{organizationId, userId}],
+        });
+        const operation = retry.operation(retryOptions);
+
+        return new Promise((resolve, reject) => {
+            operation.attempt(async (currentAttempt) => {
+                try {
+                    const organizationMembership = await this.client.organizations.deleteOrganizationMembership({ organizationId, userId });
+                    this.logger.logOutput({
+                        functionName,
+                        output: organizationMembership,
+                    });
+                    resolve(organizationMembership);
+                } catch(error) {
+                    if(operation.retry(error) && error.status && retryStatuses.includes(error.status)) {
+                        this.logger.logRetryError({
+                            functionName,
+                            currentAttempt,
+                            error,
+                        });
+                        return;
+                    }
+                    this.logger.logError({
+                        functionName,
+                        error,
+                    });
+                    reject(operation.mainError());
+                }
+            });
+        });
+    }
+
+    public async updateOrganizationMembership({
+        organizationId,
+        userId,
+        role,
+    }: {
+        organizationId: string;
+        userId: string;
+        role: OrganizationMembershipRole;
+    }) {
+        const functionName = 'clerk.organizations.updateOrganizationMembership';
+        this.logger.logInput({
+            functionName,
+            args: [{organizationId, userId, role}],
+        });
+        const operation = retry.operation(retryOptions);
+
+        return new Promise((resolve, reject) => {
+            operation.attempt(async (currentAttempt) => {
+                try {
+                    const organizationMembership = await this.client.organizations.updateOrganizationMembership({ organizationId, userId, role });
+                    this.logger.logOutput({
+                        functionName,
+                        output: organizationMembership,
+                    });
+                    resolve(organizationMembership);
+                } catch(error) {
+                    if(operation.retry(error) && error.status && retryStatuses.includes(error.status)) {
+                        this.logger.logRetryError({
+                            functionName,
+                            currentAttempt,
+                            error,
+                        });
+                        return;
+                    }
+                    this.logger.logError({
+                        functionName,
+                        error,
+                    });
+                    reject(operation.mainError());
+                }
+            });
+        });
+    }
+
+    public async createOrganization({
+        name,
+        slug,
+        createdBy,
+        maxAllowedMemberships,
+        publicMetadata,
+        privateMetadata,
+    }: {
+        name: string;
+        slug?: string;
+        createdBy?: string;
+        maxAllowedMemberships?: number;
+        publicMetadata?: OrganizationPublicMetadata;
+        privateMetadata?: OrganizationPrivateMetadata;
+    }) {
+        const functionName = 'clerk.organizations.createOrganization';
+        this.logger.logInput({
+            functionName,
+            args: [{name, slug, createdBy, maxAllowedMemberships, publicMetadata, privateMetadata}],
+        });
+        const operation = retry.operation(retryOptions);
+
+        return new Promise((resolve, reject) => {
+            operation.attempt(async (currentAttempt) => {
+                try {
+                    const organization = await this.client.organizations.createOrganization({ name, slug, createdBy, maxAllowedMemberships, publicMetadata, privateMetadata });
                     this.logger.logOutput({
                         functionName,
                         output: organization,
                     });
                     resolve(organization);
+                } catch(error) {
+                    if(operation.retry(error) && error.status && retryStatuses.includes(error.status)) {
+                        this.logger.logRetryError({
+                            functionName,
+                            currentAttempt,
+                            error,
+                        });
+                        return;
+                    }
+                    this.logger.logError({
+                        functionName,
+                        error,
+                    });
+                    reject(operation.mainError());
+                }
+            });
+        });
+    }
+
+    public async updateOrganizationLogo(organizationId: string, {
+        file,
+        uploaderUserId,
+    }: {
+        file: Blob | File;
+        uploaderUserId?: string;
+    }) {
+        const functionName = 'clerk.organizations.updateOrganizationLogo';
+        this.logger.logInput({
+            functionName,
+            args: [{organizationId, file}],
+        });
+
+        const operation = retry.operation(retryOptions);
+
+        return new Promise((resolve, reject) => {
+            operation.attempt(async (currentAttempt) => {
+                try {
+                    const organization = await this.client.organizations.updateOrganizationLogo(organizationId, { file, uploaderUserId });
+                    this.logger.logOutput({
+                        functionName,
+                        output: organization,
+                    });
+                    resolve(organization);
+                } catch(error) {
+                    if(operation.retry(error) && error.status && retryStatuses.includes(error.status)) {
+                        this.logger.logRetryError({
+                            functionName,
+                            currentAttempt,
+                            error,
+                        });
+                        return;
+                    }
+                    this.logger.logError({
+                        functionName,
+                        error,
+                    });
+                    reject(operation.mainError());
+                }
+            });
+        });
+    }
+
+    public async updateOrganization(organizationId: string, {
+        name,
+        slug,
+        publicMetadata,
+        privateMetadata,
+    }: {
+        name?: string;
+        slug?: string;
+        publicMetadata?: OrganizationPublicMetadata;
+        privateMetadata?: OrganizationPrivateMetadata;
+    }): Promise<Organization> {
+        const functionName = 'clerk.organizations.updateOrganization';
+        this.logger.logInput({
+            functionName,
+            args: [{organizationId, name, slug, publicMetadata, privateMetadata}],
+        });
+        const operation = retry.operation(retryOptions);
+
+        return new Promise((resolve, reject) => {
+            operation.attempt(async (currentAttempt) => {
+                try {
+                    const organization = await this.client.organizations.updateOrganization(organizationId, { name, slug, publicMetadata, privateMetadata });
+                    this.logger.logOutput({
+                        functionName,
+                        output: organization,
+                    });
+                    resolve(organization);
+                } catch(error) {
+                    if(operation.retry(error) && error.status && retryStatuses.includes(error.status)) {
+                        this.logger.logRetryError({
+                            functionName,
+                            currentAttempt,
+                            error,
+                        });
+                        return;
+                    }
+                    this.logger.logError({
+                        functionName,
+                        error,
+                    });
+                    reject(operation.mainError());
+                }
+            });
+        });
+    }
+
+    public async deleteOrganization(organizationId: string): Promise<Organization> {
+        const functionName = 'clerk.organizations.deleteOrganization';
+        this.logger.logInput({
+            functionName,
+            args: [{organizationId}],
+        });
+        const operation = retry.operation(retryOptions);
+
+        return new Promise((resolve, reject) => {
+            operation.attempt(async (currentAttempt) => {
+                try {
+                    const organization = await this.client.organizations.deleteOrganization(organizationId);
+                    this.logger.logOutput({
+                        functionName,
+                        output: organization,
+                    });
+                    resolve(organization);
+                } catch(error) {
+                    if(operation.retry(error) && error.status && retryStatuses.includes(error.status)) {
+                        this.logger.logRetryError({
+                            functionName,
+                            currentAttempt,
+                            error,
+                        });
+                        return;
+                    }
+                    this.logger.logError({
+                        functionName,
+                        error,
+                    });
+                    reject(operation.mainError());
+                }
+            });
+        });
+    }
+
+    public async createOrganizationMembership({
+        organizationId,
+        userId,
+        role,
+    }: {
+        organizationId: string;
+        userId: string;
+        role: OrganizationMembershipRole;
+    }): Promise<OrganizationMembership> {
+        const functionName = 'clerk.organizations.createOrganizationMembership';
+        this.logger.logInput({
+            functionName,
+            args: [{organizationId, userId, role}],
+        });
+
+        const operation = retry.operation(retryOptions);
+
+        return new Promise((resolve, reject) => {
+            operation.attempt(async (currentAttempt) => {
+                try {
+                    const organizationMembership = await this.client.organizations.createOrganizationMembership({ organizationId, userId, role });
+                    this.logger.logOutput({
+                        functionName,
+                        output: organizationMembership,
+                    });
+                    resolve(organizationMembership);
+                } catch(error) {
+                    if(operation.retry(error) && error.status && retryStatuses.includes(error.status)) {
+                        this.logger.logRetryError({
+                            functionName,
+                            currentAttempt,
+                            error,
+                        });
+                        return;
+                    }
+                    this.logger.logError({
+                        functionName,
+                        error,
+                    });
+                    reject(operation.mainError());
+                }
+            });
+        });
+    }
+
+    public async getOrganizationList({
+        includeMembersCount,
+        query,
+        limit,
+        offset,
+    }: {
+        includeMembersCount?: boolean;
+        query?: string;
+        limit?: number;
+        offset?: number;
+    }): Promise<PaginatedResourceResponse<Organization[]>> {
+        const functionName = 'clerk.organizations.getOrganizationList';
+        this.logger.logInput({
+            functionName,
+            args: [{includeMembersCount, query, limit, offset}],
+        });
+        const operation = retry.operation(retryOptions);
+
+        return new Promise((resolve, reject) => {
+            operation.attempt(async (currentAttempt) => {
+                try {
+                    const organizationsPage = await this.client.organizations.getOrganizationList({ includeMembersCount, query, limit, offset });
+                    this.logger.logOutput({
+                        functionName,
+                        output: organizationsPage,
+                    });
+                    resolve(organizationsPage);
                 } catch(error) {
                     if(operation.retry(error) && error.status && retryStatuses.includes(error.status)) {
                         this.logger.logRetryError({
--- src/users-api.ts
+++ src/users-api.ts
@@ -1,9 +1,11 @@
 import {
     ClerkClient as _ClerkClient,
+    User,
 } from '@clerk/backend';
 import { retryStatuses, retryOptions } from './utils';
 import * as retry from 'retry';
 import { ClerkLogger } from './clerk-logger.interface';
+import { PaginatedResourceResponse } from '@clerk/backend/dist/api/resources/Deserializer';
 
 export class UsersAPI {
     private client: _ClerkClient;
@@ -14,7 +16,7 @@ export class UsersAPI {
         this.logger = logger;
     }
 
-    public async getUser(userId: string) {
+    public async getUser(userId: string): Promise<User> {
         const functionName = 'clerk.users.getUser';
         this.logger.logInput({
             functionName,
@@ -45,6 +47,112 @@ export class UsersAPI {
                         error,
                     });
                     reject(operation.mainError());
+                }
+            });
+        });
+    }
+
+    public async createUser({
+        externalId,
+        emailAddress,
+        phoneNumber,
+        username,
+        password,
+        firstName,
+        lastName,
+        skipPasswordChecks,
+        skipPasswordRequirement,
+        skipLegalChecks,
+        legalAcceptedAt,
+        totpSecret,
+        backupCodes,
+        createdAt,
+    }: {
+        externalId?: string;
+        emailAddress?: string[];
+        phoneNumber?: string[];
+        username?: string;
+        password?: string;
+        firstName?: string;
+        lastName?: string;
+        skipPasswordChecks?: boolean;
+        skipPasswordRequirement?: boolean;
+        skipLegalChecks?: boolean;
+        legalAcceptedAt?: Date;
+        totpSecret?: string;
+        backupCodes?: string[];
+        createdAt?: Date;
+    }): Promise<User> {
+        const functionName = 'clerk.users.createUser';
+        this.logger.logInput({
+            functionName,
+            args: [{externalId, emailAddress, phoneNumber, username, password, firstName, lastName, skipPasswordChecks, skipPasswordRequirement, skipLegalChecks, legalAcceptedAt, totpSecret, backupCodes, createdAt}],
+        });
+        const operation = retry.operation(retryOptions);
+
+        return new Promise((resolve, reject) => {
+            operation.attempt(async (currentAttempt) => {
+                try {
+                    const createdUser = await this.client.users.createUser({externalId, emailAddress, phoneNumber, username, password, firstName, lastName, skipPasswordChecks, skipPasswordRequirement, skipLegalChecks, legalAcceptedAt, totpSecret, backupCodes, createdAt});
+                    this.logger.logOutput({
+                        functionName,
+                        output: createdUser,
+                    });
+                    resolve(createdUser);
+                } catch(error) {
+                    if(operation.retry(error) && error.status && retryStatuses.includes(error.status)) {
+                        this.logger.logRetryError({
+                            functionName,
+                            currentAttempt,
+                            error,
+                        });
+                        return;
+                    }
+                    this.logger.logError({
+                        functionName,
+                        error,
+                    });
+                    reject(operation.mainError());
+                }
+            });
+        });
+    }
+
+    public async getUserList({
+        organizationId,
+    }: {
+        organizationId?: string[];
+    }): Promise<PaginatedResourceResponse<User[]>> {
+        const functionName = 'clerk.users.getUserList';
+        this.logger.logInput({
+            functionName,
+            args: [{organizationId}],
+        });
+        const operation = retry.operation(retryOptions);
+
+        return new Promise((resolve, reject) => {
+            operation.attempt(async (currentAttempt) => {
+                try {
+                    const userList = await this.client.users.getUserList({organizationId});
+                    this.logger.logOutput({
+                        functionName,
+                        output: userList,
+                    });
+                    resolve(userList);
+                } catch(error) {
+                    if(operation.retry(error) && error.status && retryStatuses.includes(error.status)) {
+                        this.logger.logRetryError({
+                            functionName,
+                            currentAttempt,
+                            error,
+                        });
+                        return;
+                    }
+                    this.logger.logError({
+                        functionName,
+                        error,
+                    });
+                    reject(error);
                 }
             });
         });
